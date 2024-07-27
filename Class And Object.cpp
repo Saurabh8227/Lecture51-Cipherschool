5430b3b1c12eaa4c01b0c2e2003115279f8dd48b
Saurabh Pandey <119298223+Saurabh8227@users.noreply.github.com>
2024-07-27 12:50:27 +0530

    Add files via upload

---
+++ Class And Object.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class student
+{
+    public:
+
+    string name;
+    int mid;
+    float ends;
+    bool pre_abs;
+};
+class vehicle
+{
+    public:
+    string car_name;
+    string tyre_name;
+    int number_of_seats;
+
+};
+
+int mamin(){
+    student a,b,c; // here a,b,c are of type student.
+
+    int d,e,f; // here d,e,f are of int type.
+
+    //a,b,c over here are objects of the particular class.
+}
